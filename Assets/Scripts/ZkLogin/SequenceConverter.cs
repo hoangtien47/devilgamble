2c224c949e1650ffbe009fbeca24c1ce33120c1b
hoangtien47 <hoangtien472003@gmail.com>
2025-05-21 13:19:24 +0700

    add sound manager

--- Assets/Scripts/ZkLogin/SequenceConverter.cs
+++ Assets/Scripts/ZkLogin/SequenceConverter.cs
@@ -1,53 +1,53 @@
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using OpenDive.BCS;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+//using Newtonsoft.Json;
+//using Newtonsoft.Json.Linq;
+//using OpenDive.BCS;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
 
-public class SequenceConverter : JsonConverter
-{
-    public override bool CanConvert(Type objectType)
-    {
-        return objectType == typeof(Sequence);
-    }
+//public class SequenceConverter : JsonConverter
+//{
+//    public override bool CanConvert(Type objectType)
+//    {
+//        return objectType == typeof(Sequence);
+//    }
 
-    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-    {
-        // Read the array from JSON
-        JArray jArray = JArray.Load(reader);
+//    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+//    {
+//        // Read the array from JSON
+//        JArray jArray = JArray.Load(reader);
 
-        // For simple string arrays
-        if (jArray.Count > 0 && jArray[0].Type == JTokenType.String)
-        {
-            // Convert all items to BString objects
-            var bStrings = jArray.Select(item => new BString(item.ToString())).ToArray();
-            return new Sequence(bStrings);
-        }
-        // For array of arrays (like the 'b' field)
-        else if (jArray.Count > 0 && jArray[0].Type == JTokenType.Array)
-        {
-            // Create an array of Sequence objects
-            var sequenceArray = new List<ISerializable>();
+//        // For simple string arrays
+//        if (jArray.Count > 0 && jArray[0].Type == JTokenType.String)
+//        {
+//            // Convert all items to BString objects
+//            var bStrings = jArray.Select(item => new BString(item.ToString())).ToArray();
+//            return new Sequence(bStrings);
+//        }
+//        // For array of arrays (like the 'b' field)
+//        else if (jArray.Count > 0 && jArray[0].Type == JTokenType.Array)
+//        {
+//            // Create an array of Sequence objects
+//            var sequenceArray = new List<ISerializable>();
 
-            foreach (var innerArray in jArray)
-            {
-                // Convert the inner array to BString objects
-                var innerBStrings = innerArray.Select(item => new BString(item.ToString())).ToArray();
-                // Create a Sequence for this inner array
-                sequenceArray.Add(new Sequence(innerBStrings));
-            }
+//            foreach (var innerArray in jArray)
+//            {
+//                // Convert the inner array to BString objects
+//                var innerBStrings = innerArray.Select(item => new BString(item.ToString())).ToArray();
+//                // Create a Sequence for this inner array
+//                sequenceArray.Add(new Sequence(innerBStrings));
+//            }
 
-            // Create a Sequence containing Sequences
-            return new Sequence(sequenceArray.ToArray());
-        }
+//            // Create a Sequence containing Sequences
+//            return new Sequence(sequenceArray.ToArray());
+//        }
 
-        // Default: empty sequence
-        return new Sequence(new ISerializable[0]);
-    }
+//        // Default: empty sequence
+//        return new Sequence(new ISerializable[0]);
+//    }
 
-    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-    {
-        throw new NotImplementedException("Writing Sequence to JSON is not implemented.");
-    }
-}
+//    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+//    {
+//        throw new NotImplementedException("Writing Sequence to JSON is not implemented.");
+//    }
+//}
--- Assets/Scripts/_SciptableObjects/GoogleAuthConfig.cs
+++ Assets/Scripts/_SciptableObjects/GoogleAuthConfig.cs
@@ -1,38 +1,38 @@
-using UnityEngine;
+//using UnityEngine;
 
-namespace ZkLogin
-{
-    [CreateAssetMenu(fileName = "GoogleAuthConfig", menuName = "ZkLogin/Google Auth Config")]
-    public class GoogleAuthConfig : ScriptableObject
-    {
-        [Header("OAuth Configuration")]
-        [Tooltip("Your Google OAuth 2.0 Client ID")]
-        public string clientId = "YOUR_CLIENT_ID.apps.googleusercontent.com";
+//namespace ZkLogin
+//{
+//    [CreateAssetMenu(fileName = "GoogleAuthConfig", menuName = "ZkLogin/Google Auth Config")]
+//    public class GoogleAuthConfig : ScriptableObject
+//    {
+//        [Header("OAuth Configuration")]
+//        [Tooltip("Your Google OAuth 2.0 Client ID")]
+//        public string clientId = "YOUR_CLIENT_ID.apps.googleusercontent.com";
 
-        [Header("WebGL Configuration")]
-        [Tooltip("URL of your WebGL host for redirect")]
-        public string webGLHost = "https://yourgame.com";
+//        [Header("WebGL Configuration")]
+//        [Tooltip("URL of your WebGL host for redirect")]
+//        public string webGLHost = "https://yourgame.com";
 
-        [Tooltip("Path to the auth callback page")]
-        public string callbackPath = "/auth-callback.html";
+//        [Tooltip("Path to the auth callback page")]
+//        public string callbackPath = "/auth-callback.html";
 
-        [Header("Editor Testing")]
-        [Tooltip("Port for local testing in editor")]
-        public int editorPort = 8080;
+//        [Header("Editor Testing")]
+//        [Tooltip("Port for local testing in editor")]
+//        public int editorPort = 8080;
 
-        [Header("OAuth Scopes")]
-        [Tooltip("Space-separated list of OAuth scopes")]
-        public string scopes = "email profile openid";
+//        [Header("OAuth Scopes")]
+//        [Tooltip("Space-separated list of OAuth scopes")]
+//        public string scopes = "email profile openid";
 
-        public string GetRedirectUri()
-        {
-#if UNITY_EDITOR
-            return $"http://localhost:{editorPort}/oauth2redirect";
-#elif UNITY_WEBGL
-                return $"{webGLHost}{callbackPath}";
-#else
-                return $"{webGLHost}{callbackPath}";
-#endif
-        }
-    }
-}
+//        public string GetRedirectUri()
+//        {
+//#if UNITY_EDITOR
+//            return $"http://localhost:{editorPort}/oauth2redirect";
+//#elif UNITY_WEBGL
+//                return $"{webGLHost}{callbackPath}";
+//#else
+//                return $"{webGLHost}{callbackPath}";
+//#endif
+//        }
+//    }
+//}
